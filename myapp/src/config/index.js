53cad8b06089d97b295629926e11288ae802c9e6
Alexander Ignacz <alex.ignacz412@gmail.com>
2018-01-21 18:24:36 +0800

    server update

--- myapp/src/config/index.js
+++ myapp/src/config/index.js
@@ -2,7 +2,7 @@ export default {
   loggedToken: '',
   language: 0,
   // baseUrl: 'http://localhost:3000',
-  baseUrl: 'http://52.201.249.181:3000',
+  baseUrl: 'http://108.61.119.143:3000',
   swiperDuration: 5000,
   editProfile: false,
   imagePreviewUrl: '',
